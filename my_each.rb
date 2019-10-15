87a2d0f00562c7845884df7510c5275b70c675b2
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 05:06:24 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
 def my_each(array) # put argument(s) here
   # code here
-  # i = 0
-  # while i < array.length 
-  #   yield array[i]
-  # i += 1
-  # end
-  # array
+  if block_given?
+  i = 0
+  while i < array.length 
+    yield array[i]
+  i += 1
+  end
+  end
+  array
 end
